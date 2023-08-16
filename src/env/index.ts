8775e8f773e1af6c6fadbb1308e4c36a88b51aef
anderrf <anderson_rocha_farias@outlook.com>
2023-08-16 10:31:01 -0300

    chore: creation of user table and integration to docker compose

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,7 +3,8 @@ import {z} from 'zod'
 
 const envSchema = z.object({
     NODE_ENV: z.enum(['development', 'test', 'production']).default('development'),
-    PORT: z.coerce.number().default(3333)
+    PORT: z.coerce.number().default(3333),
+    DATABASE_URL: z.string()
 })
 
 const _env = envSchema.safeParse(process.env)
