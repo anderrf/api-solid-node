75bd3e64fe61f0e838cc209b6e5bc63932c6487b
anderrf <anderson_rocha_farias@outlook.com>
2023-08-16 15:09:10 -0300

    chore: implementation of repository pattern

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,5 +1,6 @@
-import { prisma } from '@/lib/prisma'
-import { hash } from 'bcryptjs'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
+import { RegisterUseCase } from '@/use-cases/register'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 
@@ -10,20 +11,15 @@ export async function register(request: FastifyRequest, reply: FastifyReply): Pr
         password: z.string().min(6)
     })
     const {name, email, password} = registerBodySchema.parse(request.body)
-    const userWithSameEmail = await prisma.user.findUnique({
-        where: {
-            email
-        }
-    })
-    if(userWithSameEmail){
-        return reply.status(409).send()
+    try{
+        const usersRepository = new  PrismaUsersRepository()
+        const registerUseCase = new RegisterUseCase(usersRepository)
+        await registerUseCase.execute({name, email, password})
     }
-    await prisma.user.create({
-        data: {
-            name,
-            email,
-            password_hash: await hash(password, 6)
+    catch(err){
+        if(err instanceof UserAlreadyExistsError){
+            return reply.status(409).send({message: err.message})
         }
-    })
+    }
     return reply.status(201).send()
 }
