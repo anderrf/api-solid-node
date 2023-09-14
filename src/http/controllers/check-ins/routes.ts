a515563396392c949db8b8db27de661258380b91
anderrf <anderson_rocha_farias@outlook.com>
2023-09-14 17:05:12 -0300

    feat: addition of RBAC to allow admin-only route acess

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -4,11 +4,16 @@ import { create } from './create'
 import { validate } from './validate'
 import { metrics } from './metrics'
 import { history } from './history'
+import { verifyUserRole } from '@/http/middlewares/verify-user-role'
 
 export async function checkInsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
   app.post('/gyms/:gymId/check-in', create)
   app.get('/check-ins/history', history)
   app.get('/check-ins/metrics', metrics)
-  app.patch('/check-ins/:checkInId/validate', validate)
+  app.patch(
+    '/check-ins/:checkInId/validate',
+    { onRequest: [verifyUserRole('ADMIN')] },
+    validate,
+  )
 }
--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -16,7 +16,7 @@ export async function authenticate(
     const authenticateUseCase = makeAuthenticateUseCase()
     const { user } = await authenticateUseCase.execute({ email, password })
     const token = await reply.jwtSign(
-      {},
+      { role: user.role },
       {
         sign: {
           sub: user.id,
@@ -24,7 +24,7 @@ export async function authenticate(
       },
     )
     const refreshToken = await reply.jwtSign(
-      {},
+      { role: user.role },
       {
         sign: { sub: user.id, expiresIn: '7d' },
       },
